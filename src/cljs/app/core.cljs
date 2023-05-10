756efb8f0e5c5b106d3b2b7f359107b7dd26ed75
vlopatka <vadim.lopatka@gmail.com>
2023-05-10 21:11:20 +0300

    added helix lib for react

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,6 +1,23 @@
-(ns app.core)
+(ns app.core 
+  (:require [helix.core :refer [defnc $]]
+            [helix.hooks :as hooks]
+            [helix.dom :as d]
+            ["react-dom/client" :as rdom]
+            ))
+
+(defnc app []
+  (let [[state set-state] (hooks/use-state {:url ""})]
+    (d/div
+      (d/input {:value (:url state)
+                :on-change #(set-state assoc :url (.. % -target -value))})
+     (d/button "Shorten URL"))))
+
+
+  ;; start your app with your favorite React renderer
+;; (defonce root (rdom/createRoot (js/document.getElementById "app")))
+;; (.render root ($ app))
 
 (defn ^:export init []
-  (js/console.log "howzit"))
+  (let [root (rdom/createRoot (js/document.getElementById "app"))]
+    (.render root ($ app))))
 
-;; (js/alert "Hello, Anastasiia!!!")
